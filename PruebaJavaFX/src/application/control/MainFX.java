31a2feef43035f54e26ed2c86169e58ed490b56d
javier <javierliebana99@gmail.com>
2018-12-05 18:26:14 +0100

    1º commit, falta que funcione el metodo init para el fade trasition y el tooltip

---
+++ PruebaJavaFX/src/application/control/MainFX.java
@@ -0,0 +1,49 @@
+package application.control;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+
+
+
+public class MainFX extends Application {
+private AnchorPane mypane;
+
+
+	
+	@Override
+	public void start(Stage stage) throws IOException {
+
+		// primero cargamos la vista fxml en el FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Profile.fxml"));
+	
+		mypane = (AnchorPane) loader.load();
+		
+		// añade la escena al stage y la titula
+		stage.setTitle("Ventana Main");
+		stage.setScene(new Scene(mypane));
+		
+		stage.show();
+		
+		Font.loadFont(
+				getClass().getResourceAsStream(
+						"assets/PoiretOne-Regular.ttf"), 20);
+		
+		
+		
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+}
